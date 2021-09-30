50214fec261051b3f0f202c4ffc94932cddf4421
kaidalena <elenanikolaevna999@gmail.com>
2021-09-30 17:29:58 +0300

    step by step by https://developer.mozilla.org/ru/docs/Learn/Server-side/Django
    parts 1-6

---
+++ catalog/views.py
@@ -0,0 +1,50 @@
+from django.shortcuts import render
+from .models import Book, Author, BookInstance, Genre
+from django.views import generic
+
+
+def index(request):
+    """
+    Функция отображения для домашней страницы сайта.
+    """
+    # Генерация "количеств" некоторых главных объектов
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+    # Доступные книги (статус = 'a')
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+    num_authors = Author.objects.count()  # Метод 'all()' применён по умолчанию.
+    num_genres = Genre.objects.count()
+
+    # Отрисовка HTML-шаблона index.html с данными внутри
+    # переменной контекста context
+    return render(
+        request,
+        'index.html',
+        context={
+            'num_books': num_books,
+            'num_instances': num_instances,
+            'num_instances_available': num_instances_available,
+            'num_authors': num_authors,
+            'num_genres': num_genres
+        },
+    )
+
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 10
+    # context_object_name = 'my_book_list'   # ваше собственное имя переменной контекста в шаблоне
+
+    # def get_queryset(self):
+    #     return Book.objects.filter(title__icontains='war')[:5]  # Получить 5 книг, содержащих 'war' в заголовке
+
+    # def get_context_data(self, **kwargs):
+    #     # В первую очередь получаем базовую реализацию контекста
+    #     context = super(BookListView, self).get_context_data(**kwargs)
+    #     # Добавляем новую переменную к контексту и инициализируем её некоторым значением
+    #     context['some_data'] = 'This is just some data'
+    #     return context
+
+
+class BookDetailView(generic.DetailView):
+    model = Book
